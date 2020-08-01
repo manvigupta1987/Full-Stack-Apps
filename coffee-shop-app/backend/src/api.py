7aed3571b8da75034f854169013e635c2b657a04
Manvi <manvigupta1987@gmail.com>
2020-07-31 23:14:45 -0400

    CoffeeShop: Added end points

--- coffee-shop-app/backend/src/api.py
+++ coffee-shop-app/backend/src/api.py
@@ -12,7 +12,6 @@ setup_db(app)
 CORS(app)
 
 '''
-@TODO uncomment the following line to initialize the datbase
 !! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 '''
@@ -31,7 +30,6 @@ def after_request(response):
 
 ## ROUTES
 '''
-@TODO implement endpoint
     GET /drinks
         it should be a public endpoint
         it should contain only the drink.short() data representation
@@ -57,7 +55,6 @@ def get_drinks():
 
 
 '''
-@TODO implement endpoint
     GET /drinks-detail
         it should require the 'get:drinks-detail' permission
         it should contain the drink.long() data representation
@@ -83,7 +80,6 @@ def get_drinks_detail(payload):
 
 
 '''
-@TODO implement endpoint
     POST /drinks
         it should create a new row in the drinks table
         it should require the 'post:drinks' permission
@@ -109,7 +105,6 @@ def create_drink(payload):
 
 
 '''
-@TODO implement endpoint
     PATCH /drinks/<id>
         where <id> is the existing model id
         it should respond with a 404 error if <id> is not found
@@ -119,6 +114,8 @@ def create_drink(payload):
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
         or appropriate status code indicating reason for failure
 '''
+
+
 @app.route('/drinks/<id>', methods=['PATCH'])
 @requires_auth('patch:drinks')
 def update_drink(payload, id):
@@ -138,8 +135,8 @@ def update_drink(payload, id):
     except:
         abort(500)
 
+
 '''
-@TODO implement endpoint
     DELETE /drinks/<id>
         where <id> is the existing model id
         it should respond with a 404 error if <id> is not found
@@ -152,6 +149,19 @@ def update_drink(payload, id):
 '''
 Example error handling for unprocessable entity
 '''
+
+
+@app.route('/drinks/<id>', methods=['DELETE'])
+@requires_auth('delete:drinks')
+def delete_drink(payload, id):
+    try:
+        drink = Drink.query.filter(Drink.id == id).one_or_none()
+        if not drink:
+            abort(404)
+        drink.delete()
+        return jsonify({'success': True, 'delete': id}), 200
+    except:
+        abort(500)
 
 
 @app.errorhandler(422)
